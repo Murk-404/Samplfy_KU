b9553ea58dd0af8ed461d5fa679d6e749857f499
Murk_404 <fullmtyl@gmail.com>
2022-09-18 18:54:14 -0500

    reset merge

--- client/src/pages/Top Data Page/top-data.js
+++ client/src/pages/Top Data Page/top-data.js
@@ -3,55 +3,74 @@ import React, {useState, useEffect} from 'react'
 import {collection, addDoc, Timestamp, query, orderBy, onSnapshot} from "firebase/firestore"
 import {db} from './/../../firebase-db'
 import Credentials from '../../Credentials/Credentials'
-// import axios from 'axios'
+import axios from 'axios'
+// const express = require('express');
 
 const Song = () => {
+  // const [CODE, setCODE] = useState([])
+  // const addCode = () => {
+  //   const code = localStorage.getItem('code');
+  //   try {
+  //       addDoc(collection(db, 'users'), {
+  //       code: code,
+  //       created: Timestamp.now()
+  //     })
+  //     // onClose()
+  //   } catch (err) {
+  //     alert(err)
+  //   }
+  //   return
+  // }
 
+  // window.addEventListener('load', (event) => {
+  //   addCode()
+  // });
+
+  // async function readCode() {
+  //   const q = query(collection(db, 'users'), orderBy('created', 'desc'))
+  //   onSnapshot(q, (querySnapshot) => {
+  //     setCODE(querySnapshot.docs.map(doc => ({
+  //       id: doc.id,
+  //       data: doc.data()
+  //     })))
+  //   })
+  // }
+  // const runSomething = () => {
+  //   readCode().then(
+  //     function() { 
+  //       console.log('This is the code')
+  //       console.log(CODE[0].data.code)
+  //     }
+  //   )
+  // }
   const CLIENT_ID = Credentials.CLIENT_ID;
   const REDIRECT_URI = Credentials.REDIRECT_URI;
   const CLIENT_SECRET = Credentials.CLIENT_SECRET;
 
-  const [CODE, setCODE] = useState([])
-  const addCode = () => {
-    const code = localStorage.getItem('code');
-    try {
-        addDoc(collection(db, 'users'), {
-        code: code,
-        created: Timestamp.now()
-      })
-      // onClose()
-    } catch (err) {
-      alert(err)
-    }
-    return
-  }
+  const AUTH_CODE = localStorage.getItem('code');
 
-  window.addEventListener('load', (event) => {
-    addCode()
-  });
-
-  async function readCode() {
-    const q = query(collection(db, 'users'), orderBy('created', 'desc'))
-    onSnapshot(q, (querySnapshot) => {
-      setCODE(querySnapshot.docs.map(doc => ({
-        id: doc.id,
-        data: doc.data()
-      })))
-    })
-  }
-  const runSomething = () => {
-    readCode().then(
-      function() { 
-        console.log('This is the code')
-        console.log(CODE[0].data.code)
+  const getAccessToken = () => {
+    axios({
+      method: 'post',
+      url: 'https://accounts.spotify.com/api/token',
+      params: {
+        code: AUTH_CODE,
+        redirect_uri: REDIRECT_URI,
+        grant_type: 'authorization_code'
+      },
+      headers: {
+        'Content-Type': 'applicaton/x-www-form-urlencoded',
+        'Authorization': 'Basic ' + window.btoa(`${CLIENT_ID}:${CLIENT_SECRET}`)
       }
-    )
+    })
   }
   
   return (
     <div id='container'>
       <div id='text'>
-        <button onClick={ runSomething }> Read Code </button>
+        {/* <button onClick={ runSomething }> Read Code </button> */}
+        <button onClick={ getAccessToken }> Get Access Token </button>
+
       </div>
     </div>
   );
