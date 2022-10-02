32d35a5c2e582b9e649d49761be69364db394c85
Murk_404 <fullmtyl@gmail.com>
2022-10-01 20:42:57 -0500

    Server Changes

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const port = 5000;
 
 app.listen(port, () => console.log('listening on port ' + port));
 app.get('/api', (req, res) => {
+
   const child = spawner('python', ['C:/Spotify_React_Project/server/spot_auth.py']);
   child.stdout.on('data', (data) => {
     console.log(`This is the data: ${data}`)
@@ -19,3 +20,22 @@ app.get('/api', (req, res) => {
   });
 });
 
+// const { authorize } = require('@spotifly/auth-token');
+
+// const SCOPE = 'user-library-read, ugc-image-upload, user-modify-playback-state, user-read-playback-state, user-read-currently-playing, user-follow-modify, user-follow-read, user-read-recently-played, user-read-playback-position, user-top-read, playlist-read-collaborative, playlist-modify-public, playlist-read-private, playlist-modify-private, app-remote-control, streaming, user-read-email, user-read-private, user-library-modify, user-library-read'
+
+// SPOTIPY_CLIENT_ID = '53c0537d418540e08a9f37fa33e35052'
+// SPOTIPY_CLIENT_SECRET = '3fc6ed86ebf6482291f9ad061aa3d5d1'
+
+// async function asyncCall() {
+//   const token = await authorize({
+//     clientId: `${SPOTIPY_CLIENT_ID}`,
+//     clientSecret: `${SPOTIPY_CLIENT_SECRET}`,
+//     port: 5000,
+//     scopes: `${SCOPE}`,
+//     noEmit: true,
+//   });
+//   console.log(token);
+// }
+// console.log(asyncCall());
+// console.log(token);
--- server/spot_auth copy copy.py
+++ server/spot_auth copy copy.py
@@ -1,23 +1,54 @@
 import spotipy
-# from spotipy import CacheHandler
-# from spotipy.oauth2 import SpotifyAuthBase
-# import json
+from spotipy import CacheHandler
+# from spotipy.oauth2 import SpotifyClientCredentials
+import json
 from firebase import Firebase
 import firebase
+
+# class MemoryCacheHandler(CacheHandler):
+#     """
+#     A cache handler that simply stores the token info in memory as an
+#     instance attribute of this class. The token info will be lost when this
+#     instance is freed.
+#     """
+
+#     def __init__(self, token_info=None):
+#         """
+#         Parameters:
+#             * token_info: The token info to store in memory. Can be None.
+#         """
+#         self.token_info = token_info
+
+#     def get_cached_token(self):
+#         return self.token_info
+
+#     def save_token_to_cache(self, token_info):
+#         self.token_info = token_info
 
 exec(open("C:\Spotify_React_Project\server\env.py").read())
 SCOPE = open('C:\Spotify_Project\website-files\Spotify_Auth\scopes.txt').read()
 
 top_data = []
-cache_handler = spotipy.cache_handler.MemoryCacheHandler()
-oauth_manager = spotipy.oauth2.SpotifyOAuth(cache_handler=cache_handler, scope=SCOPE)
-token = spotipy.util.prompt_for_user_token(oauth_manager=oauth_manager)
-SPOTIFY = spotipy.Spotify(auth=token)
 
+token = spotipy.util.prompt_for_user_token(scope=SCOPE)
+SPOTIFY = spotipy.Spotify(auth=token)
+# username = spotipy.Spotify(auth=token).current_user()['uri'].replace('spotify:user:', '')
 username = SPOTIFY.current_user()['id']
 pfp = SPOTIFY.current_user()['images'][0]['url']
 name = SPOTIFY.current_user()['display_name']
 
+# for i in range(3):
+#   top_data = top_data.append(SPOTIFY.current_user_top_tracks(limit=20, offset=(i*20), time_range='medium_term'))
+# termArr = [
+#   'short_term',
+#   'medium_term',
+#   'long_term'
+# ]
+# dataArr = [
+#   SPOTIFY.current_user_top_tracks(limit=50, offset=0, time_range=termArr[0]), 
+#   SPOTIFY.current_user_top_tracks(limit=50, offset=0, time_range=termArr[1]), 
+#   SPOTIFY.current_user_top_tracks(limit=50, offset=0, time_range=termArr[2])
+# ]
 top_tracks_short = SPOTIFY.current_user_top_tracks(limit=49, offset=0, time_range='short_term')
 top_tracks_medium = SPOTIFY.current_user_top_tracks(limit=49, offset=0, time_range='medium_term')
 top_tracks_long = SPOTIFY.current_user_top_tracks(limit=49, offset=0, time_range='long_term')
@@ -56,6 +87,8 @@ config  = {
 
 firebase = Firebase(config)
 db = firebase.database()
+data = {"token": token}
+db.child(username).child("user-token").set(data)
 db.child(username).child("top-tracks").child("top-tracks-short").set(top_tracks_short)
 db.child(username).child("top-tracks").child("top-tracks-medium").set(top_tracks_medium)
 db.child(username).child("top-tracks").child("top-tracks-long").set(top_tracks_long)
@@ -63,4 +96,7 @@ db.child(username).child("top-artists").child("top-artists-short").set(top_artis
 db.child(username).child("top-artists").child("top-artists-medium").set(top_artists_medium)
 db.child(username).child("top-artists").child("top-artists-long").set(top_artists_long)
   
+# print(type(top_data))
+# print(top_data2['items'][0])
+# print(len(top_data1['items']))
 print(username + '$' + pfp + '$' + name)
