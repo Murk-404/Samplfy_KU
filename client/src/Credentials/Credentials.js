02c31f21f4d1a816ff0aee7a5c81d3098c44bb04
Murk-404 <105297858+Murk-404@users.noreply.github.com>
2022-10-15 13:03:31 -0500

    Removed Credentials

--- client/src/Credentials/Credentials.js
+++
@@ -1,7 +0,0 @@
-const Credentials = {
-  CLIENT_ID : "53c0537d418540e08a9f37fa33e35052",
-  CLIENT_SECRET : "3fc6ed86ebf6482291f9ad061aa3d5d1",
-  REDIRECT_URI : "http://localhost:3000"
-}
-
-export default Credentials;
